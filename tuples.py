31551f5b2adfa207d540adacd6c53f3c67003e1c
sharon <sharonsujee36@gmail.com>
2019-04-19 22:33:42 +0530

    tuples.py added

---
+++ tuples.py
@@ -0,0 +1,6 @@
+# Author : Sharon Sahadevan
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    my_tuple = tuple(integer_list)
+    print(hash(my_tuple))
